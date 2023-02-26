711ecb07d07321dde690f54657c08896015ced92
Belyakov <sano.belyakov@mail.ru>
2023-02-26 15:02:39 +0300

    first and second tasks

--- Home work-2 OOP files.py
+++ Home work-2 OOP files.py
@@ -1,16 +1,31 @@
-# This is a sample Python script.
+with open("recipesbook.txt", "r", encoding="utf-8") as file:
+    cook_book_dict = {}
+    # cook_book_keys = []
+    for line in file:
+        dish_name = line.strip()
+        counter = int(file.readline())
+        dish_ingr_list = []
+        for _ in range(counter):
+            ingr = file.readline().strip()
+            ingredient_name, quantity, measure = ingr.split(" | ")
+            dish_ingr_list.append({"ingredient_name": ingredient_name, "quantity": quantity, "measure": measure})
+        cook_book_dict[dish_name] = dish_ingr_list
+        file.readline()
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def get_shop_list_by_dishes(dishes, person_count):
+    shop_list = {}
+    for dish in dishes:
+        for ing in cook_book_dict[dish]:
+            ing_name = ing["ingredient_name"]
+            if ing_name not in shop_list:
+                shop_list[ing_name] = {"quantity": int(ing["quantity"]) * person_count, "measure": ing["measure"]}
+            else:
+                shop_list[ing_name]["quantity"] += int(ing["quantity"]) * person_count
+    return shop_list
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from pprint import pprint
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+pprint(cook_book_dict, sort_dicts=False)
+print(get_shop_list_by_dishes(["Омлет", "Фахитос"], 3))
